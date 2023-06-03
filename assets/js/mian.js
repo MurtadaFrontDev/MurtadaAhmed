c7d8299bcb84affb782ff479908623b1373124c4
murtada <murtada.ah2006@gmail.com>
2023-06-03 04:40:41 +0300

    Nav bar

---
+++ assets/js/mian.js
@@ -0,0 +1,13 @@
+// Make toogle of nav bar
+
+// 1- make var
+let barsIcon = document.getElementById("bars");
+let slidNavBar = document.getElementById("slid_nav_bar");
+let closeSileBar = document.getElementById("close_slide_bar");
+
+barsIcon.addEventListener("click", function () {
+  slidNavBar.classList.add("toogle_slide_bar");
+});
+closeSileBar.addEventListener("click", function () {
+  slidNavBar.classList.remove("toogle_slide_bar");
+});
